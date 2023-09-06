c34720d539959be1ae631887602c29f59080d1ed
unknown <fabioflauzino@hotmail.com>
2023-09-05 23:38:58 -0300

    subindo projeto

---
+++ src/data/questions.ts
@@ -0,0 +1,24 @@
+import { Questions } from "@/types/Questions";
+
+export const questions: Questions[] = [
+  {
+    question: 'Qual ingrediente não pode faltar',
+    options: [
+      'Farinha',
+      'Corante',
+      'Água',
+      'Açucar'
+    ],
+    answer: 1
+  },
+  {
+    question: 'Qual o melhor método de cozinhar',
+    options: [
+      'Microondas',
+      'Fogão',
+      'Fogueira',
+      'Lança-chamas'
+    ],
+    answer: 3
+  }
+]
